6bdbe34bc650b3c831006f1a04eb1854dc4c6f97
kangCoder <khsofficial1213@gmail.com>
2022-06-29 21:43:06 +0900

    user edit 수정2

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
-  PORT: process.env.PORT,
 });
 
 db.connect((err) => {
