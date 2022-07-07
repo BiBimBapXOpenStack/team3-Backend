c64bc9f2f4d86a2bc109200176fed7f3a91c87e4
kangCoder <khsofficial1213@gmail.com>
2022-07-08 00:32:41 +0900

    DB env 환경변수 처리

--- bin/setup.sh
+++ bin/setup.sh
@@ -3,11 +3,6 @@
 # @param $1=DB_HOST, $2=DB_USER, $3=DB_PASSWORD, $4=DB_DATABASE, $5=DB_PORT, $6=DEV_SERVER_URL
 
 echo "==========================================="
-echo "0. Stop nginx"
-echo "==========================================="
-sudo systemctl stop nginx
-
-echo "==========================================="
 echo "1. Install Dependency"
 echo "==========================================="
 sudo apt-get update
@@ -43,11 +38,6 @@ echo "4. Build"
 echo "==========================================="
 cd ~/team3-Backend/server
 sudo npm install
-export HOST=$1
-export USER=$2
-export PASSWORD=$3
-export DATABASE=$4
-export PORT=$5
 sudo npm install pm2 -g
 pm2 start app.js
 
